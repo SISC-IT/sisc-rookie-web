338c1f4a5260f7b4d561bee67010622dbf289e5e
realcdh <rdhdevelop@gmail.com>
2026-05-14 14:25:04 +0900

    feat: enhance member entity constraints

--- src/main/java/com/sisc_it/sisc_rookie_web/domain/Member.java
+++ src/main/java/com/sisc_it/sisc_rookie_web/domain/Member.java
@@ -1,6 +1,9 @@
 package com.sisc_it.sisc_rookie_web.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -12,16 +15,34 @@ public class Member {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
-    
+
+    @Column(nullable = false, unique = true)
     private String email;
 
-    public Member() {
+    @Column(nullable = false)
+    private String password;
+
+    private String profileImageUrl;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    protected Member() {
     }
 
-    public Member(String name, String email) {
+    public Member(String name, String email, String password, Role role) {
         this.name = name;
         this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public Member(String name, String email, String password, String profileImageUrl, Role role) {
+        this(name, email, password, role);
+        this.profileImageUrl = profileImageUrl;
     }
 
     public Long getId() {
@@ -46,5 +67,29 @@ public class Member {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getProfileImageUrl() {
+        return profileImageUrl;
+    }
+
+    public void setProfileImageUrl(String profileImageUrl) {
+        this.profileImageUrl = profileImageUrl;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
     }
 }
