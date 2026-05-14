c5a1cccde775140bb184d0cb557ac99691a54b31
hwangyunsang51-byte <hwangyunsang51@gmail.com>
2026-05-14 18:26:30 +0900

    featn knjknkjnjkkj kjbkbkj knkjn

--- src/main/java/com/sisc_it/sisc_rookie_web/domain/Member.java
+++ src/main/java/com/sisc_it/sisc_rookie_web/domain/Member.java
@@ -7,8 +7,12 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
 
 @Entity
+@Getter
+
+
 public class Member {
 
     @Id
@@ -45,51 +49,4 @@ public class Member {
         this.profileImageUrl = profileImageUrl;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getProfileImageUrl() {
-        return profileImageUrl;
-    }
-
-    public void setProfileImageUrl(String profileImageUrl) {
-        this.profileImageUrl = profileImageUrl;
-    }
-
-    public Role getRole() {
-        return role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
-    }
 }
