6943c1ba59e4d970e8d5d8286fcef3a02d9fcd82
kimseungjun <qps8622@gmail.com>
2026-05-31 23:58:36 +0900

    refactor: ApiResponse 수정
    응답 코드 추가
    성공 시 응답코드(200)은 따로 보내지 않음 : null
    에러 발생 시 데이터를 보내지 않는 것으로 임시 설정

--- src/main/java/com/sisc_it/sisc_rookie_web/global/response/ApiResponse.java
+++ src/main/java/com/sisc_it/sisc_rookie_web/global/response/ApiResponse.java
@@ -1,16 +1,24 @@
 package com.sisc_it.sisc_rookie_web.global.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record ApiResponse<T>(
     int status,
     String message,
+    String code,
     T data
 ) {
 
+    // 성공 시에는 커스텀 에러 코드가 필요 없으므로 code 자리에 null을 넣습니다.
     public static <T> ApiResponse<T> success(int status, String message, T data) {
-        return new ApiResponse<>(status, message, data);
+        return new ApiResponse<>(status, null, message, data);
     }
 
-    public static <T> ApiResponse<T> error(int status, String message) {
-        return new ApiResponse<>(status, message, null);
+    // 데이터(data)는 없고, 상태코드, 커스텀코드, 메시지만 줍니다.
+    public static <T> ApiResponse<T> error(int status, String code, String message) {
+        return new ApiResponse<>(status, code, message, null);
     }
+
+
 }
