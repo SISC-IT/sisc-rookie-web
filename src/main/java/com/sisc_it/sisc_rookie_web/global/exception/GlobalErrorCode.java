425c564ce372ca7c05dad902652aece914d9cfa2
kimseungjun <qps8622@gmail.com>
2026-05-31 23:54:17 +0900

    refactor: ErrorCode 인터페이스 분리 및 GlobalException 구현
    ErrorCode : 인터페이스 -> 다형성
    GlobalException : 우리가 정의한 Exception 외의 표준 예외 처리(3개)

--- src/main/java/com/sisc_it/sisc_rookie_web/global/exception/GlobalErrorCode.java
+++ src/main/java/com/sisc_it/sisc_rookie_web/global/exception/GlobalErrorCode.java
@@ -1,4 +1,3 @@
-// 공통 글로벌 에러
 package com.sisc_it.sisc_rookie_web.global.exception;
 
 import lombok.Getter;
@@ -8,9 +7,17 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum GlobalErrorCode implements ErrorCode {
-    VALIDATION_ERROR(HttpStatus.BAD_REQUEST, "요청 값이 올바르지 않습니다."),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 오류가 발생했습니다.");
+
+    // 1. DTO 유효성 검사 및 JSON 파싱 에러용 (400)
+    VALIDATION_ERROR(HttpStatus.BAD_REQUEST, "GLB-400", "요청 데이터가 올바르지 않습니다."),
+
+    // 2. HTTP 메서드 오류용 (405)
+    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "GLB-405", "지원하지 않는 HTTP 메서드입니다."),
+
+    // 3. 최후의 보루 500 에러용
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "GLB-500", "서버 내부에 알 수 없는 오류가 발생했습니다.");
 
     private final HttpStatus status;
+    private final String code;
     private final String message;
 }
